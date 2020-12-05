87493171a2359b5e9d4607f0b9f6fed709ab3c63
Jimmy Pulido <jiapulidoar@gmail.com>
2020-12-05 07:51:37 -0500

    Add gnuplot

--- lte-multicell.cc
+++ lte-multicell.cc
@@ -9,10 +9,85 @@
 #include "ns3/applications-module.h"
 #include "ns3/point-to-point-helper.h"
 #include "ns3/config-store.h"
-
+#include "ns3/gnuplot.h"
+#include <math.h>
 using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE("LteMulticell");
+
+void plotDevices(NodeContainer Nodes, NodeContainer ueNodes, Gnuplot2dDataset dataset_graph)
+{
+
+    std::string fileNameWithNoExtension = "nodes";
+    std::string graphicsFileName = fileNameWithNoExtension + ".png";
+    std::string plotFileName = fileNameWithNoExtension + ".plt";
+    std::string plotTitle = "2-D Plot";
+    std::string dataTitle = "2-D Data";
+
+    // Instantiate the plot and set its title.
+    Gnuplot plot(graphicsFileName);
+    plot.SetTitle(plotTitle);
+
+    // Make the graphics file, which the plot file will create when it
+    // is used with Gnuplot, be a PNG file.
+    plot.SetTerminal("png");
+
+    // Set the labels for each axis.
+    plot.SetLegend("X", "Y");
+
+    // Set the range for the x axis.
+    plot.AppendExtra("set xrange [-1.5:+1.5]");
+    plot.AppendExtra("set pointsize 3");
+
+    // Instantiate the dataset, set its title, and make the points be
+    // plotted along with connecting lines.
+    Gnuplot2dDataset dataset;
+    dataset.SetTitle("eNodeB");
+    dataset.SetStyle(Gnuplot2dDataset::POINTS);
+    dataset.SetExtra("pointtype 3");
+
+    for (uint32_t u = 0; u < Nodes.GetN(); ++u)
+    {
+        Ptr<Node> ueNode = Nodes.Get(u);
+        Ptr<MobilityModel> mob = ueNode->GetObject<MobilityModel>();
+
+        dataset.Add(mob->GetPosition().x, mob->GetPosition().y);
+    }
+
+    // Add the dataset to the plot.
+    plot.AddDataset(dataset);
+
+    // ueNodes Dataset
+    Gnuplot2dDataset dataset2;
+    dataset2.SetTitle("UE");
+    dataset2.SetStyle(Gnuplot2dDataset::POINTS);
+    dataset2.SetExtra("pointtype 26");
+
+    for (uint32_t u = 0; u < ueNodes.GetN(); ++u)
+    {
+        Ptr<Node> ueNode = ueNodes.Get(u);
+        Ptr<MobilityModel> mob = ueNode->GetObject<MobilityModel>();
+
+        dataset2.Add(mob->GetPosition().x, mob->GetPosition().y);
+    }
+
+    // Add the dataset to the plot.
+    plot.AddDataset(dataset2);
+
+    // Add the dataset to the plot.
+    plot.AddDataset(dataset_graph);
+
+    
+
+        // Open the plot file.
+        std::ofstream plotFile(plotFileName.c_str());
+
+    // Write the plot file.
+    plot.GenerateOutput(plotFile);
+
+    // Close the plot file.
+    plotFile.close();
+}
 
 int main(int argc, char *argv[])
 {
@@ -21,7 +96,7 @@ int main(int argc, char *argv[])
     RngSeedManager::SetSeed(3); // Changes seed from default of 1 to 3
     RngSeedManager::SetRun(7);  // Changes run number from default of 1 to 7
 
-    uint16_t numberOfNodes = 2;
+    uint16_t numberOfNodes = 3;
     double simTime = 1.1;
     double interPacketInterval = 1;
 
@@ -31,6 +106,8 @@ int main(int argc, char *argv[])
     cmd.AddValue("simTime", "Total duration of the simulation [s])", simTime);
     cmd.AddValue("interPacketInterval", "Inter packet interval [ms])", interPacketInterval);
     cmd.Parse(argc, argv);
+
+    //float p = 1 / numberOfNodes;
 
     Ptr<LteHelper> lteHelper = CreateObject<LteHelper>();
     Ptr<PointToPointEpcHelper> epcHelper = CreateObject<PointToPointEpcHelper>();
@@ -66,8 +143,9 @@ int main(int argc, char *argv[])
 
     // Install Mobility Model
     Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator>();
-    positionAlloc->Add(Vector(-100.0, 0.0, 0.0)); //for enb 1
-    positionAlloc->Add(Vector(100.0, 0.0, 0.0));  //for enb 2
+    positionAlloc->Add(Vector(-1.0, 0.0, 0.0)); //for enb 1
+    positionAlloc->Add(Vector(1.0, 0.0, 0.0));  //for enb 2
+    positionAlloc->Add(Vector(0.0, 0.0, 0.0));  //for enb 2
 
     MobilityHelper mobility;
     mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
@@ -76,12 +154,12 @@ int main(int argc, char *argv[])
 
     if (!f_RandomPositionAllocator)
     {
-        positionAlloc->Add(Vector(-100.0, 10.0, 0.0));   //for ue 1
-        positionAlloc->Add(Vector(100.0, 10.0, 0.0));    //for ue 2
-        positionAlloc->Add(Vector(-100.0, -100.0, 0.0)); //for ue 3
-        positionAlloc->Add(Vector(100.0, -100.0, 0.0));  //for ue 4
-        positionAlloc->Add(Vector(-1.0, 0.0, 0.0));      //for ue 5
-        positionAlloc->Add(Vector(1.0, 0.0, 0.0));       //for ue 6
+        positionAlloc->Add(Vector(-1.0, 1.0, 0.0));  //for ue 1
+        positionAlloc->Add(Vector(1.0, 1.0, 0.0));   //for ue 2
+        positionAlloc->Add(Vector(-1.0, -1.0, 0.0)); //for ue 3
+        positionAlloc->Add(Vector(1.0, -1.0, 0.0));  //for ue 4
+        positionAlloc->Add(Vector(-1., 0.0, 0.0));   //for ue 5
+        positionAlloc->Add(Vector(1.0, 0.0, 0.0));   //for ue 6
 
         mobility.SetPositionAllocator(positionAlloc);
         mobility.Install(enbNodes);
@@ -90,17 +168,20 @@ int main(int argc, char *argv[])
     else
     {
         Ptr<UniformRandomVariable> x = CreateObject<UniformRandomVariable>();
-        x->SetAttribute("Min", DoubleValue(-50));
-        x->SetAttribute("Max", DoubleValue(50));
+        x->SetAttribute("Min", DoubleValue(-0.1));
+        x->SetAttribute("Max", DoubleValue(0.1));
 
         //NS_LOG_UNCOND(x->GetValue());
 
         mobility.SetPositionAllocator(positionAlloc);
         mobility.Install(enbNodes);
-        
-        mobility2.SetPositionAllocator("ns3::RandomDiscPositionAllocator", "X", DoubleValue(x->GetValue()), "Rho", StringValue("ns3::UniformRandomVariable[Min=100|Max=150]"));
+
+        mobility2.SetPositionAllocator("ns3::RandomDiscPositionAllocator", "X", DoubleValue(x->GetValue()), "Rho", StringValue("ns3::UniformRandomVariable[Min=0.9|Max=1]"));
         mobility2.Install(ueNodes);
     }
+
+    // Print position
+
 
     lteHelper->SetSchedulerType("ns3::RrFfMacScheduler");
 
@@ -122,17 +203,43 @@ int main(int argc, char *argv[])
     }
 
     // Attach one UE per eNodeB
-    for (uint16_t i = 0; i < numberOfNodes; i++)
+
+    Gnuplot2dDataset dataset_graph;
+    //dataset_graph.SetExtra("linetype 0");vec
+    dataset_graph.SetErrorBars(Gnuplot2dDataset::XY);
+
+    for (uint16_t i = 0; i < ueNodes.GetN(); i++)
     {
-        lteHelper->Attach(ueLteDevs.Get(i), enbLteDevs.Get(i));
-        lteHelper->Attach(ueLteDevs.Get(i + 2), enbLteDevs.Get(i));
-        lteHelper->Attach(ueLteDevs.Get(i + 4), enbLteDevs.Get(i));
+        Ptr<UniformRandomVariable> v = CreateObject<UniformRandomVariable>();
+
+        uint16_t enbid = ceil(v->GetValue() * numberOfNodes) - 1;
+
+        lteHelper->Attach(ueLteDevs.Get(i), enbLteDevs.Get(enbid));
+
+        // get points
+        Ptr<Node> ueNode = ueNodes.Get(i);
+        Ptr<MobilityModel> mob_ue = ueNode->GetObject<MobilityModel>();
+
+        Ptr<Node> enbNode = enbNodes.Get(enbid);
+        Ptr<MobilityModel> mob_enb = enbNode->GetObject<MobilityModel>();
+
+        double x = mob_ue->GetPosition().x;
+        double y = mob_ue->GetPosition().y;
+        double xErrorDelta = mob_enb->GetPosition().x - x;
+        double yErrorDelta = mob_enb->GetPosition().y  - y;
+
+        dataset_graph.Add(x, y, xErrorDelta, yErrorDelta);
+
+        //NS_LOG_UNCOND("ENB" << enbid + 1 << ",UE" << i + 1);
     }
+
+    plotDevices(enbNodes, ueNodes, dataset_graph);
 
     // Install and start applications on UEs and remote host
     uint16_t dlPort = 1234;
     ApplicationContainer clientApps;
     ApplicationContainer serverApps;
+
     for (uint32_t u = 0; u < ueNodes.GetN(); ++u)
     {
         PacketSinkHelper dlPacketSinkHelper("ns3::UdpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), dlPort + u));
